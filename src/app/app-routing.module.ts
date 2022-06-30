8e51cd470e886db2d703ef02ab1abec910515be1
Don Harrison <donald.harrison@utah.edu>
2022-06-30 06:04:55 -0700

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home-page', component: HomeComponent},
+  { path: 'sign-in-page', component: SignInPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
